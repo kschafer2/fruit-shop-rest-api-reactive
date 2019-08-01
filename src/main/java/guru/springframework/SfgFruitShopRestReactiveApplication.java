6f8f24bfa699ffc83d549ce1396d407f3c4e5868
Kyle Schafer <kschay@protonmail.com>
2019-08-01 11:51:24 -0500

    For #1 - Added CircleCI Config File

--- src/main/java/guru/springframework/SfgFruitShopRestReactiveApplication.java
+++ src/main/java/guru/springframework/SfgFruitShopRestReactiveApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FruitShopRestApiReactiveApplication {
+public class SfgFruitShopRestReactiveApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FruitShopRestApiReactiveApplication.class, args);
+		SpringApplication.run(SfgFruitShopRestReactiveApplication.class, args);
 	}
 
 }
--- src/test/java/guru/springframework/fruitshoprestapireactive/SfgFruitShopRestReactiveApplicationTests.java
+++ src/test/java/guru/springframework/fruitshoprestapireactive/SfgFruitShopRestReactiveApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class FruitShopRestApiReactiveApplicationTests {
+public class SfgFruitShopRestReactiveApplicationTests {
 
 	@Test
 	public void contextLoads() {
