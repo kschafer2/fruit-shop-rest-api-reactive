e3fc88f73dcd0b96d4899ffa34f275e6d595448c
Kyle Schafer <kschay@protonmail.com>
2019-08-01 13:35:43 -0500

    For #4 - Implemented Get All Categories and Get Category By Id

---
+++ src/main/java/guru/springframework/sfgfruitshoprestreactive/controllers/CategoryController.java
@@ -0,0 +1,32 @@
+package guru.springframework.sfgfruitshoprestreactive.controllers;
+
+
+import guru.springframework.sfgfruitshoprestreactive.domain.Category;
+import guru.springframework.sfgfruitshoprestreactive.repositories.CategoryRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/v1/categories")
+public class CategoryController {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryController(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @GetMapping
+    public Flux<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Mono<Category> getCategoryById(@PathVariable String id) {
+        return categoryRepository.findById(id);
+    }
+}
---
+++ src/test/java/guru/springframework/sfgfruitshoprestreactive/controllers/CategoryControllerTest.java
@@ -0,0 +1,52 @@
+package guru.springframework.sfgfruitshoprestreactive.controllers;
+
+import guru.springframework.sfgfruitshoprestreactive.domain.Category;
+import guru.springframework.sfgfruitshoprestreactive.repositories.CategoryRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import static org.mockito.BDDMockito.given;
+
+public class CategoryControllerTest {
+
+    public static final String ID = "id";
+    WebTestClient webTestClient;
+    CategoryRepository categoryRepository;
+    CategoryController categoryController;
+
+    @Before
+    public void setUp() throws Exception {
+        categoryRepository = Mockito.mock(CategoryRepository.class);
+        categoryController = new CategoryController(categoryRepository);
+        webTestClient = WebTestClient.bindToController(categoryController).build();
+    }
+
+    @Test
+    public void getAllCategoriesTest() {
+        given(categoryRepository.findAll())
+                .willReturn(Flux.just(
+                Category.builder().name("name1").build(),
+                Category.builder().name("name2").build()));
+
+        webTestClient.get()
+                .uri("/api/v1/categories")
+                .exchange()
+                .expectBodyList(Category.class)
+                .hasSize(2);
+    }
+
+    @Test
+    public void getCategoryByIdTest() {
+        given(categoryRepository.findById(ID))
+                .willReturn(Mono.just(Category.builder().name("name").build()));
+
+        webTestClient.get()
+                .uri("/api/v1/categories" + ID)
+                .exchange()
+                .expectBody(Category.class);
+    }
+}
